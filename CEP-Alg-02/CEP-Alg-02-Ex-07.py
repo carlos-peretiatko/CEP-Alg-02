cf4a99f15f11605961b01eab3f70a02bf32b0a72
Eduardo <carloseduardoperetiatko07@gmail.com>
2026-03-19 13:54:10 -0300

    7 beta

--- CEP-Alg-02/CEP-Alg-02-Ex-07.py
+++ CEP-Alg-02/CEP-Alg-02-Ex-07.py
@@ -2,3 +2,11 @@
 # algarismo das centenas, D é o algarismo das dezenas e U o algarismo das unidades) Faça um
 # programa Python que receba do usuário o número inteiro N, e imprima separadamente a
 # centena, a dezena e a unidade.
+
+n = int(input("Informe o numero desejado: "))
+
+c = n // 100
+d = (n % 100) // 10
+u = d % 10
+
+print("D: ", c ,"\nD: ", d ,"\nU: ", u)
